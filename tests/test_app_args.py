4815c4c0316e5da279e2123c090dc8e0464bb6ce
Jeff Buttars <jeffbuttars@gmail.com>
2018-05-22 08:09:30 -0600

    print stuff

--- tests/test_app_args.py
+++ tests/test_app_args.py
@@ -5,8 +5,13 @@ from .tools import cur_dir
 def test_app_args_empty():
     res = app_args(cur_dir())
 
-    print('test_app_args', res)
     assert res
     assert res.get('components') == []
     assert res.get('event_hooks') == []
     assert res.get('routes') == []
+
+
+def test_app_args_print():
+    # Make sure it doesn't explode
+    res = app_args(cur_dir(), print_results=True)
+    assert res
