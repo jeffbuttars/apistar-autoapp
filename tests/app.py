431e4816a5ac1369a1549ac178da1a09f09067b5
Jeff Buttars <jeffbuttars@gmail.com>
2018-05-23 07:57:15 -0600

    Add an `app.py` file in tests to cover that path in `find_caller_dir`

---
+++ tests/app.py
@@ -0,0 +1,17 @@
+from apistar import App, ASyncApp
+
+from apistar_autoapp import AutoApp, AutoASyncApp
+
+
+# Adds coverage for the 'Using from a file named app.py' scenario, which
+# is typical
+def test_from_app_file():
+    print('test_from_app_file')
+
+    app = AutoApp()
+    assert isinstance(app, App)
+    assert app.event_hooks == []
+
+    app = AutoASyncApp()
+    assert isinstance(app, ASyncApp)
+    assert app.event_hooks == []
--- tests/test_entry.py
+++ tests/test_entry.py
@@ -2,6 +2,8 @@ from apistar import App, ASyncApp
 
 from apistar_autoapp import AutoApp, AutoASyncApp
 
+from .app import test_from_app_file
+
 
 def test_empty_autoApp():
     app = AutoApp()
@@ -13,3 +15,7 @@ def test_empty_autoASyncApp():
     app = AutoASyncApp()
     assert isinstance(app, ASyncApp)
     assert app.event_hooks == []
+
+
+def test_app_entry():
+    test_from_app_file()
